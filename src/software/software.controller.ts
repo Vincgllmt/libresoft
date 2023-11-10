487ce5fe0f534c33d7c29d8ce572fcc1db8b013d
Alexis Udycz <alexis.udycz@etudiant.univ-reims.fr>
2023-11-10 08:08:11 +0000

    feat: :sparkles: added number of users on software

--- src/software/software.controller.ts
+++ src/software/software.controller.ts
@@ -2,9 +2,24 @@ import { NextFunction, Request, Response } from "express";
 import { softwareCollection } from "./software.collection";
 export class SoftwareController {
     static async list(req: Request, res: Response, next: NextFunction) {
-        const softwares = await softwareCollection.find().limit(20).toArray()
+        const softwares = await softwareCollection.aggregate([
+            {
+                $project:
+                {
+                    usersCount:
+                    {
+                        $size: "$users"
+                    },
+                    name: 1,
+                    id: 1,
+                    url: 1,
+                    description: 1,
+                    external_resources: 1
+
+                }
+            }
+        ]).limit(20).toArray()
         console.log(softwares)
-        res.render('software/software_list', {softwares})
+        res.render('software/software_list', { softwares })
     }
 }
-        

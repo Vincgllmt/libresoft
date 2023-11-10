a6415a5a33f2ae3acb77562c22b96b647f9a0fcc
Alexis Udycz <alexis.udycz@etudiant.univ-reims.fr>
2023-11-10 09:10:30 +0000

    feat: :construction: working on pagination

--- src/software/software.controller.ts
+++ src/software/software.controller.ts
@@ -1,25 +1,38 @@
 import { NextFunction, Request, Response } from "express";
 import { softwareCollection } from "./software.collection";
+import { validationResult } from "express-validator";
 export class SoftwareController {
     static async list(req: Request, res: Response, next: NextFunction) {
-        const softwares = await softwareCollection.aggregate([
-            {
-                $project:
+        const result = validationResult(req);
+        if (result.isEmpty() && req.query.page) {
+            const softwares = await softwareCollection.aggregate([
                 {
-                    usersCount:
-                    {
-                        $size: "$users"
+                    $project: {
+                        usersCount:
+                        {
+                            $size: "$users"
+                        },
+                        name: 1,
+                        id: 1,
+                        url: 1,
+                        description: 1,
+                        external_resources: 1,
                     },
-                    name: 1,
-                    id: 1,
-                    url: 1,
-                    description: 1,
-                    external_resources: 1
-
+                },
+                {
+                    $facet: {
+                        total: [{ $count: "total_softwares" }],
+                        softwares: [
+                            { $skip: ((+req.query.page - 1) * 20) },
+                            { $limit: 20 }
+                        ]
+                    }
                 }
-            }
-        ]).limit(20).toArray()
-        console.log(softwares)
-        res.render('software/software_list', { softwares })
+            ]).toArray()
+            console.dir(softwares, { depth: null })
+            res.render('software/software_list', { softwares: softwares[0].softwares, total: Math.ceil(softwares[0].total[0].total_softwares / 20), page: req.query.page })
+        }else{
+            throw new Error("Invalid query")
+        }
     }
 }
--- src/software/software.routes.ts
+++ src/software/software.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { SoftwareController } from "./software.controller";
 import expressAsyncHandler from 'express-async-handler'
+import { query } from "express-validator";
 const router = Router();
 
-router.get('/', expressAsyncHandler(SoftwareController.list))
+router.get('/', query("page").default(1).isNumeric().isLength({min: 1}), expressAsyncHandler(SoftwareController.list))
 
 export default router;

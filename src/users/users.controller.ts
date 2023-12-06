12936e87780b13e2dac3d110c0e3cb9b962e028e
Alexis Udycz <alexis.udycz@etudiant.univ-reims.fr>
2023-12-06 07:49:18 +0000

    feat: distinct users list

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,29 +1,20 @@
 import { NextFunction, Request, Response } from "express";
 import { softwareCollection } from "../software/software.collection";
-export class UsersController{
-    static async list(req: Request, res: Response, next: NextFunction){
-        let software: Software | {users: SoftwareUsers[]} | null   = null
+export class UsersController {
+    static async list(req: Request, res: Response, next: NextFunction) {
+        let software: Software | { users: SoftwareUsers[] } | null = null
 
         if (req.params.id !== undefined) {
-            const softwaresResult = await softwareCollection.find<Software>({id: +req.params.id}).toArray()
-            if(softwaresResult.length > 0) {
+            const softwaresResult = await softwareCollection.find<Software>({ id: +req.params.id }).toArray()
+            if (softwaresResult.length > 0) {
                 software = softwaresResult[0]
             }
-            
-        }else{
-            const uniqueUserIds = await softwareCollection.distinct('users')
-            const uniqueUsers = await softwareCollection.aggregate<Software>([{
-                $group: {
-                    
-                }
-              }, {
-                $project: {
-                    users: 1
-                }
-              }]).toArray();
-            software = {users: uniqueUsers.flatMap(x=>x.users)};
-              
-            console.log(uniqueUsers);
+
+        } else {
+            software = {
+                users: (await softwareCollection.distinct('users') as SoftwareUsers[]).sort((a, b) => a.name.localeCompare(b.name))
+            }
+            console.dir(software, { depth: null });
             //console.log(uniqueUser)
             // software = await softwareCollection.find<Software>({users: {id: {$in: uniqueUser}}}).toArray()
             //software = await softwareCollection.aggregate<Software>([{

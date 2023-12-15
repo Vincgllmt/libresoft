c5c7fea66a3b6a42c4bb667b52698c31227c86db
Alexis Udycz <alexis.udycz@etudiant.univ-reims.fr>
2023-12-15 14:42:54 +0000

    feat: :sparkles: usage of redis for session data cache

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,19 @@ import softwareRouter from './software/software.routes'
 import usersRouter from './users/users.routes'
 import authRouter from './auth/auth.route';
 import { sessionUser } from './auth/auth.middleware';
+import RedisStore from 'connect-redis';
+import { redis } from './services/redis';
 
 const app = express()
 const port = process.env.PORT ?? 8000;
 
 app.set('view engine', 'ejs');
-
+const redisStore = new RedisStore({client: redis, prefix: 'session:'})
 app.use(session({
-    secret: process.env.session_secret ?? "Pourquoi feldup alors que feldown", // ajoutez la variable d'environnement correspondante au fichier .env
+    secret: process.env.session_secret ?? "Pourquoi feldup alors que feldown | keyboard cat", // ajoutez la variable d'environnement correspondante au fichier .env
     saveUninitialized: false,
-    resave: false
+    resave: false,
+    store: redisStore,
 }));
 
 app.use(express.static('public'));
